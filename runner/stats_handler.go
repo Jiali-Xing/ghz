f5aaf2f1a4b93deb49adcbac19504cdddca86689
邢嘉力 Jiali Xing <jiali.xing@protonmail.com>
2023-07-03 00:01:09 -0400

    [Debug test] tried hard to get the ratelimited req fail properly

--- runner/stats_handler.go
+++ runner/stats_handler.go
@@ -2,6 +2,7 @@ package runner
 
 import (
 	"context"
+	"fmt"
 	"sync"
 
 	"google.golang.org/grpc/stats"
@@ -41,6 +42,12 @@ func (c *statsHandler) HandleRPC(ctx context.Context, rs stats.RPCStats) {
 		c.lock.RUnlock()
 
 		if !ign {
+			// extract the Trailer metadata.MD from rs and print it
+			metadata := rs.Trailer
+			for k, v := range metadata {
+				fmt.Printf("Trailer: %s: %s\n", k, v)
+			}
+
 			duration := rs.EndTime.Sub(rs.BeginTime)
 
 			var st string
